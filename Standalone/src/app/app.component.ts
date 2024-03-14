5830dee86bdd6ca0e665c604313d2a1b069f0aed
harshshukla2002 <harshshukla2312@gmail.com>
2024-03-14 22:32:40 +0530

    Learning: Standalone Component

--- Standalone/src/app/app.component.ts
+++ Standalone/src/app/app.component.ts
@@ -1,12 +1,13 @@
-import { Component } from "@angular/core";
-import { RouterModule } from "@angular/router";
-import { WelcomeComponent } from "./welcome/welcome.component";
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+import { WelcomeComponent } from './welcome/welcome.component';
 
 @Component({
   standalone: true,
   imports: [WelcomeComponent, RouterModule],
-  selector: "app-root",
-  templateUrl: "./app.component.html",
-  styleUrls: ["./app.component.css"],
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {}
--- Standalone/src/main.ts
+++ Standalone/src/main.ts
@@ -1,10 +1,10 @@
-import { enableProdMode, importProvidersFrom } from "@angular/core";
-import { bootstrapApplication } from "@angular/platform-browser";
+import { enableProdMode, importProvidersFrom } from '@angular/core';
+import { bootstrapApplication } from '@angular/platform-browser';
 
-import { AppComponent } from "./app/app.component";
+import { AppRoutingModule } from './app/app-routing.module';
+import { AppComponent } from './app/app.component';
 // import { AnalyticsService } from './app/shared/analytics.service';
-import { environment } from "./environments/environment";
-import { AppRoutingModule } from "./app/app-routing.module";
+import { environment } from './environments/environment';
 
 if (environment.production) {
   enableProdMode();
@@ -13,6 +13,6 @@ if (environment.production) {
 bootstrapApplication(AppComponent, {
   providers: [
     // AnalyticsService,
-    importProvidersFrom(AppRoutingModule),
+    importProvidersFrom(AppRoutingModule)
   ],
 });
